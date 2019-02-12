7a6e8aa58703f3c00957cfa1d63730a50b89d150
Ariel Vitor Molina <arielvmol@terra.com.br>
2019-02-11 23:38:04 -0200

    Correção de layout.

--- CadastroFamilia/App_Start/BundleConfig.cs
+++ CadastroFamilia/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace CadastroFamilia
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
